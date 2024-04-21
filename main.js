3137637d0bfeea922cc78103f1d1b447bf306dd8
William Whitaker <william@williamwhitaker.dev>
2024-04-21 09:46:53 -0400

    adds comments

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 import Color from "colorjs.io";
 
+// Definitions
 const colorIn = document.querySelector("#color-in");
 const colorAdd = document.querySelector("#color-add");
 const colorOut = document.querySelector("#color-out");
 
+// Initialize variables
 let color;
 let swatches = [];
 
+// Functions
+function createSwatch(v) {
+	const newSwatch = document.createElement("div");
+	newSwatch.className = "small";
+	newSwatch.style.backgroundColor = v;
+	colorOut.insertAdjacentElement("beforeend", newSwatch);
+}
+
+// Event listeners
 colorIn.addEventListener("input", function () {
+	// Check that the value from the input field is a valid
+	// format. If it is, assign it to `color`.
 	if (colorIn.checkValidity()) {
 		color = new Color(colorIn.value);
 	} else {
@@ -17,22 +30,14 @@ colorIn.addEventListener("input", function () {
 });
 
 colorAdd.addEventListener("click", function () {
+	// editor flags `toString` b/c it thinks it is native js
 	swatches.push(color.toString( {format: "hex" }));
 	createSwatch(color);
 });
-
-function createSwatch(v) {
-	const newSwatch = document.createElement("div");
-	newSwatch.className = "small";
-	newSwatch.style.backgroundColor = v;
-	colorOut.insertAdjacentElement("beforeend", newSwatch);
-}
 
 document.addEventListener("keyup", (event) => {
 	const keyName = event.key;
 	if ( keyName === "Enter" ) {
 		createSwatch(color);
-	} else {
-		return;
 	}
 })
